0d03fa39b9ad7786050dd84c5cd4dda61d204382
ElijahPavlov <capelinx@gmail.com>
2022-08-17 19:51:44 +0300

    create component lk.jsx

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -10,6 +10,8 @@ const Layout = ({children}) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Bike_Routes</title>
             <script defer src="/js/client.js"></script>
+            <link rel="stylesheet" href="/css/bootstrap.min.css" />
+        <script defer src="/js/bootstrap.bundle.min.js" />
         </head>
         <body>
             {children}
