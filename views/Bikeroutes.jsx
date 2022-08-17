205cf1a0edcee66384cd9803ecb146a3e53f93e2
Vadim <akulenko_vadim@mail.ru>
2022-08-17 21:36:33 +0300

    test card (WIP)

--- views/Bikeroutes.jsx
+++ views/Bikeroutes.jsx
@@ -4,9 +4,16 @@ const Layout = require('./Layout');
 function BikeRoutes() {
   return (
     <Layout>
-      <div>Название маршрута</div>
+      <div>
+        <h3>Название маршрута:</h3>
+        <h4>Локация:</h4>
+        <p><img src="images/dzen.png" alt="Тест" /></p>
+        <p>Автор:</p>
+        <p>Протяженность:</p>
+        <h6><a href="http://www.yandex.ru">Подробнее</a></h6>
+      </div>
     </Layout>
   )
-}
+};
 
 module.exports = BikeRoutes;
