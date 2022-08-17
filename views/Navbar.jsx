1d07b1bcde789f418b4e32d35f919389aaa7b11c
ElijahPavlov <capelinx@gmail.com>
2022-08-17 18:45:57 +0300

    added bootstrap script and navbar component

---
+++ views/Navbar.jsx
@@ -0,0 +1,31 @@
+const React = require('react');
+const Layout = require('./Layout');
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Bike_Rootes</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Registration</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/auth/login">Login</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">########</a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+module.exports = Navbar;
