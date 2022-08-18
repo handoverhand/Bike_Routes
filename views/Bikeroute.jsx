95d24e1dd4452c2d77ad591a1d7c1a70eb48e09f
Vadim <akulenko_vadim@mail.ru>
2022-08-18 17:54:42 +0300

    critical updates

--- views/Bikeroute.jsx
+++ views/Bikeroute.jsx
@@ -4,7 +4,7 @@ function BikeRoute({ id, title, location, riderLoginBR, length }) {
   return (
     <div className="cardElement">
       <br />
-      <h3>Название маршрута: {title}</h3>
+      <h3>Название маршрута: «{title}»</h3>
       <h4>Локация: {location}</h4>
       <h5>Протяженность: {length} км</h5>
       <p><img src="/images/test.png" alt="Тест" width={500} /></p>
