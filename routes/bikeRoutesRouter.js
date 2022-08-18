d0ec33da32e142d7de231a704236067791b8a4ea
SAMorozov <s.a.morozov93@yandex.ru>
2022-08-18 21:15:30 +0300

    Params maps updated

--- routes/bikeRoutesRouter.js
+++ routes/bikeRoutesRouter.js
@@ -2,11 +2,30 @@ const bikeRoutesRouter = require('express').Router();
 const Bikerouteslist = require('../views/Bikerouteslist');
 const { Bikeroute } = require('../db/models');
 
+const InfoRoute = require('../views/InfoRoute');
+
 bikeRoutesRouter.get('/', async (req, res) => {
   const allRoutes = await Bikeroute.findAll({
     raw: true,
   });
   res.renderComponent(Bikerouteslist, { allRoutes });
+});
+
+bikeRoutesRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const [ bikerouteId ] = await Bikeroute.findAll({raw: true, where: { id } });
+  res.renderComponent(InfoRoute, { id, bikerouteId});
+});
+
+bikeRoutesRouter.get('/:id/info', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [ bikerouteId ] = await Bikeroute.findAll({raw: true, where: { id } });
+    console.log('bikeroute', bikerouteId);
+    res.json(bikerouteId);
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 module.exports = bikeRoutesRouter;
