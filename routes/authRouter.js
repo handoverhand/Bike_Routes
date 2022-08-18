72695f1d0a737867b7ec833dbb58d70aa5f74b10
KrAnatoly <kranatoly@gmail.com>
2022-08-18 14:23:56 +0300

    Password comparsing using bcrypt

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,6 @@
 const authRouter = require('express').Router();
 const bcrypt = require('bcrypt');
+const { useReducer } = require('react');
 const AuthorizationForm = require('../views/Authorization.jsx');
 const { Rider } = require('../db/models');
 
@@ -8,12 +9,35 @@ authRouter.get('/log', (req, res) => {
 });
 
 authRouter.post('/log', async (req, res) => {
+  let rider;
   try {
-    const user = await Rider.findOne({ where: { login: req.body.login } });
-    res.json({ error: null, user})
+    rider = await Rider.findOne({ where: { login: req.body.login } });
+    if (!rider) {
+      res.json({ error: 'Такого пользователя или пароля не существует!' });
+      return;
+    }
   } catch (error) {
     res.json({ error: error.message });
+    return;
   }
+  try {
+    const isSame = await bcrypt.compare(req.body.password, rider.password);
+    //console.log(req.body.password, rider.password, rider.login, req.body.login);
+
+    if (!isSame) {
+      res.json({ error: 'Такого пользователя или пароля не существует' });
+      return;
+    }
+  } catch (error) {
+    res.json({ error: error.message });
+    // return
+  }
+
+  req.session.rider = {
+    id: rider.id,
+    login: rider.login,
+  };
+  res.redirect('/');
 });
 
 module.exports = authRouter;
