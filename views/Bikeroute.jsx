d30a07bf6f2364828f26d01e55b85e00c5fbaa6b
KrAnatoly <kranatoly@gmail.com>
2022-08-19 12:45:18 +0300

    Flexbox-style

--- views/Bikeroute.jsx
+++ views/Bikeroute.jsx
@@ -2,16 +2,16 @@ const React = require('react');
 
 function BikeRoute({ id, title, location, riderLoginBR, length, img}) {
   return (
-    <div className="cardElement">
-      <br />
-      <h3>Маршрут: «{title}»</h3>
-      <h4>Локация: {location}</h4>
-      <h5>Протяженность: {length} км</h5>
-      <p><img src={img} alt="Тест" width={500} /></p>
-      <p>Автор: {riderLoginBR}</p>
-      <button className="btn btn-primary"><a href={`/bikeroutes/${id}`}><p className = "bikeReg">Подробнее</p></a></button>
-      <br />
-    </div>
+    <form type="submit" className="cardElement" action={`/bikeroutes/${id}`}>
+    <br />
+    <h3>Маршрут: «{title}»</h3>
+    <h4>Локация: {location}</h4>
+    <h5>Протяженность: {length} км</h5>
+    <p><img src={img} alt="Тест" width={500} /></p>
+    <p>Автор: {riderLoginBR}</p>
+    <button type="submit" className="btn btn-primary">Подробнее</button>
+    <br />
+  </form>
   );
 }
 
