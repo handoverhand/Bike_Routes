46c060caf1c09d1a72614441a89f26a0c406044b
SAMorozov <s.a.morozov93@yandex.ru>
2022-08-19 11:55:19 +0300

    Registration updated

--- views/Authorization.jsx
+++ views/Authorization.jsx
@@ -1,21 +1,22 @@
 const React = require('react');
 const Layout = require('./Layout.jsx');
 
-function AuthorizationForm() {
+function AuthorizationForm({ message }) {
   return (
     <Layout>
-      <div className = "whiteDiv"></div>
-      <h2>Для входа в личный кабинет введите Никнейм и пароль</h2>
-      <div className = "whiteDiv"></div>
-      <form action="/auth/log" method="POST">
-        <label htmlFor="loginInput" className="form-label"><h3>Введите Никнейм</h3></label>
-        <input name="login" type="text" id="loginInput" className="form-control" />
-        <br />
-        <label htmlFor="passwordInput" className="form-label"><h3>Введите пароль</h3></label>
-        <input name="password" type="password" id="passwordInput" className="form-control" />
-        <br />
-        <button type="submit" className="btn btn-primary">Вход</button>
-      </form>
+            <div className="whiteDiv" />
+            <h2>Для входа в личный кабинет введите Никнейм и пароль</h2>
+            <div className="whiteDiv" />
+            <form action="/auth/log" method="POST">
+              <label htmlFor="loginInput" className="form-label"><h3>Введите Никнейм</h3></label>
+              <input name="login" type="text" id="loginInput" className="form-control" />
+              <br />
+              <label htmlFor="passwordInput" className="form-label"><h3>Введите пароль</h3></label>
+              <input name="password" type="password" id="passwordInput" className="form-control" />
+              <br />
+              <button type="submit" className="btn btn-primary">Вход</button>
+              <div>{message}</div>
+            </form>
     </Layout>
   );
 }
