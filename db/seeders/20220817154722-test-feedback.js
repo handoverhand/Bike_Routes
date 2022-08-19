b24af7977e8821da8d1be489d7e0c568624ff6d3
Vadim <akulenko_vadim@mail.ru>
2022-08-19 15:46:12 +0300

    Table with feedback fixed

--- db/seeders/20220817154722-test-feedback.js
+++ db/seeders/20220817154722-test-feedback.js
@@ -1,21 +1,42 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('Feedbacks', [{
-      comment: 'Зашло!',
+      comment: 'Сильно не топите, пацаны',
       grade: 5,
       riderLoginFB: 'Гриша',
       routeId: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
-      comment: 'Пойдет...',
+      comment: 'Было сложно, но я вывез',
+      grade: 4,
+      riderLoginFB: 'Ваня Хворост',
+      routeId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      comment: 'Стреляют, но ездить можно',
+      grade: 3,
+      riderLoginFB: 'Топский Павел',
+      routeId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      comment: 'Салам Алейкум братьям с Ингушетии',
       grade: 3,
       riderLoginFB: 'Ваня Хворост',
       routeId: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
-      comment: 'Не очень.',
+      comment: 'Газуйте доталго, мужики',
+      grade: 3,
+      riderLoginFB: 'Гриша',
+      routeId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      comment: 'Сделаем красиво, мы же за Россию',
       grade: 2,
       riderLoginFB: 'Топский Павел',
       routeId: 3,
